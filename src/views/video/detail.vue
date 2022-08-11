03a2ece76e86f1d114d1d37a57a5218cbf9b4080
solid <solid@gmail.com>
2022-08-11 23:25:29 +0800

    修改详情页

--- src/views/video/detail.vue
+++ src/views/video/detail.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        detail
-        {{ $route.params.id }}
-        {{ videoInfo }}
+        <img class="thumb" :src="videoInfo.thumb" :alt="videoInfo.name">
+        <p class="name">{{ videoInfo.name }}</p>
+        <p class="tips">如关联了设备，则投射到设备，否则直接播放</p>
+        <div class="play-list">
+            <span class="item" v-for="(item,idx) in videoInfo.links" :key="idx">{{ item.name }}</span>
+        </div>
+        <p class="intro"><b>介绍：</b>{{ videoInfo.intro }}</p>
     </div>
 
 </template>
@@ -22,7 +26,7 @@
         },
         methods: {
             getVideoInfo(id) {
-                this.axios.get('/api/info/' + id).then((response) => {
+                this.axios.get('/api/video/detail/' + id).then((response) => {
                     console.log('[getVideoInfo.response]', response.data);
                     this.videoInfo = response.data
                 });
