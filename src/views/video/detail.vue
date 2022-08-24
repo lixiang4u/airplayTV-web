98e655767d5e0de3d28aa1233eed62fd2c23f731
solid <solid@gmail.com>
2022-08-24 20:55:09 +0800

    修改参数带路径格式问题

--- src/views/video/detail.vue
+++ src/views/video/detail.vue
@@ -8,8 +8,9 @@
             <p class="tips text-red">如关联了设备，则投射到设备，否则直接播放</p>
             <p class="intro"><b>介绍：</b>{{ videoInfo.intro }}</p>
             <div class="play-list">
-                <router-link :to="{ name: 'video-play', params: { id: item.id }, query:{'vid':videoInfo.id}}"
-                             v-for="(item,idx) in videoInfo.links" :key="idx">
+                <router-link
+                        :to="{ name: 'video-play', params: { id: encodeURIComponent(item.id) }, query:{'vid':videoInfo.id}}"
+                        v-for="(item,idx) in videoInfo.links" :key="idx">
                     <span class="item">{{ item.name }}</span>
                 </router-link>
             </div>
