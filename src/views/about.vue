e660331b0b155a7bf7199f680fa1bc411c129e66
game.desktop <game.desktop@example.com>
2024-09-25 10:31:58 +0800

    修改配置

--- src/views/about.vue
+++ src/views/about.vue
@@ -41,7 +41,7 @@
       </div>
       <div>
         <q-checkbox size="md" @click="changeSource('cz')" v-model="videoSource.cz"
-                    label="源1(cz)（废弃/需要显卡模拟绕过CF验证）"/>
+                    label="源1(cz)（质量较高，有时候需要cloudflare challenge）"/>
       </div>
       <div>
         <q-checkbox size="md" @click="changeSource('nn')" v-model="videoSource.nn"
